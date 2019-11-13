a9b40222dcb95f0794c4ece922f08949b967f6a2
Mina Kim <57683876+Minakim731@users.noreply.github.com>
2019-11-13 14:24:34 -0500

    Add files via upload

---
+++ project program file.py
@@ -0,0 +1,11 @@
+#program file
+from project import*
+a.speed(0)
+turtle.bgcolor("lightcyan")
+
+light(10,"chocolate")
+dark(10,"saddlebrown")
+black(10,"black")
+darkR(10,"darkred")
+darkpink(10,"palevioletred")
+medpink(10,"mediumvioletred")
